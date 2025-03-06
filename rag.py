26a75d2b7cde34d0a78da0a0d6406d4d2b629e72
praneethS435 <saipraneeth.sristi@gmail.com>
2025-03-06 13:23:42 +0530

    Delete rag.py

--- rag.py
+++
@@ -1,9 +0,0 @@
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-
-def get_rag_chain(vector_store):
-    retriever = vector_store.as_retriever()
-    llm = ChatOpenAI(model_name="gpt-4", temperature=0)
-    
-    qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever, return_source_documents=True)
-    return qa_chain
